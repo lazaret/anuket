8071b501fcd2eb6d03d01ce02db265b3486c8c78
Bertrand Lecervoisier <miniwark@gmail.com>
2012-02-18 14:54:36 +0100

    Change the default view to use Mako templates and Twitter Bootstrap

--- anubis/views/views.py
+++ anubis/views/views.py
@@ -1,5 +1,6 @@
 from pyramid.view import view_config
 
-@view_config(route_name='home', renderer='anubis:templates/mytemplate.pt')
+#@view_config(route_name='home', renderer='anubis:templates/index.mako')
+@view_config(route_name='home', renderer='index.mako')
 def my_view(request):
-    return {'project':'Anubis'}
+    return {'brand_name':'Anubis'}
