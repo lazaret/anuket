8467c991fc40615f3e44d161e431a2178dd7ae75
Bertrand Lecervoisier <miniwark@gmail.com>
2012-02-20 20:39:24 +0100

    Change view stucture and move routes definition in view files

--- wepwawet/__init__.py
+++ wepwawet/__init__.py
@@ -1,19 +1,16 @@
 # -*- coding: utf-8 -*-
 from pyramid.config import Configurator
-
+from .views import root
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
     config.include(add_static_views)
-    config.include(add_routes)
+    # include routes
+    config.include(root)
     config.scan()
     return config.make_wsgi_app()
 
 def add_static_views(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
-
-def add_routes(config):
-    config.add_route('home', '/')
-    config.add_route('login', '/login')
--- wepwawet/views/root.py
+++ wepwawet/views/root.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 from pyramid.view import view_config
 
+def includeme(config):
+    """Add root pages routes."""
+    config.add_route('home', '/')
+    config.add_route('login', '/login')
 
 #@view_config(route_name='home', renderer='wepwawet:templates/index.mako')
+@view_config(context='pyramid.exceptions.NotFound', renderer='404.mako')
 @view_config(route_name='home', renderer='index.mako')
-def my_view(request):
-    return {'brand_name':'Wepwawet'}
-
 @view_config(route_name='login', renderer='login.mako')
-def login_view(request):
+def root_view(request):
+    """Render the root pages."""
     return {'brand_name':'Wepwawet'}
