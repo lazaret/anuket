f8ff0af9af1a1710b031ca23f0d96d3940a390df
Bertrand Lecervoisier <miniwark@gmail.com>
2012-02-22 23:16:57 +0100

    Add the pyramid session factory

--- wepwawet/__init__.py
+++ wepwawet/__init__.py
@@ -1,30 +1,35 @@
 # -*- coding: utf-8 -*-
 from pyramid.config import Configurator
+from pyramid.session import UnencryptedCookieSessionFactoryConfig
 #from pyramid.authentication import AuthTktAuthenticationPolicy
 #from pyramid.authorization import ACLAuthorizationPolicy
 from .views import root, tools
+from .lib import subscribers
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    config = Configurator(settings=settings)
+    # configure session
+    session_factory = UnencryptedCookieSessionFactoryConfig('itsaseekreet')
+    config.set_session_factory(session_factory)
+#    # configure auth & auth
 #    authorization_policy = ACLAuthorizationPolicy()
 #    authentication_policy = AuthTktAuthenticationPolicy('seekrit') #TODO use SessionAuthenticationPolicy ?
-#    # TODO see https://bitbucket.org/cancel/pyrone/src/087372020b67/pyrone/__init__.py
-    config = Configurator(settings=settings)
-#    # set auth & auth
 #    config.set_authentication_policy(authentication_policy)
 #    config.set_authorization_policy(authorization_policy)
+
+    # configure subscribers
+    config.include(subscribers)
+    # configure static views
     config.include(add_static_views)
-    # include routes
+    # configure routes
     config.include(root)
     config.include(tools)
-    # scan views
+    # configure views
     config.scan()
-    # include subscribers
-    config.add_subscriber('wepwawet.lib.subscribers.add_renderer_globals', 'pyramid.events.BeforeRender')
-    config.add_subscriber('wepwawet.lib.subscribers.add_localizer', 'pyramid.events.NewRequest')
-    # internationalization
+    # configure i18n
     config.add_translation_dirs('wepwawet:locale')
     config.set_locale_negotiator('wepwawet.lib.i18n.locale_negotiator')
     return config.make_wsgi_app()
@@ -32,3 +37,6 @@ def main(global_config, **settings):
 
 def add_static_views(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
+
+#TODO: Auth & auth
+#TODO: decide if we use beaker for sessions
--- wepwawet/lib/subscribers.py
+++ wepwawet/lib/subscribers.py
@@ -2,6 +2,11 @@
 from pyramid.i18n import get_localizer, TranslationStringFactory
 
 
+def includeme(config):
+    """Configure subscribers."""
+    config.add_subscriber(add_renderer_globals, 'pyramid.events.BeforeRender')
+    config.add_subscriber(add_localizer, 'pyramid.events.NewRequest')
+
 def add_renderer_globals(event):
     request = event.get('request')
     event['_'] = request.translate
