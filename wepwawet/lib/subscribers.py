57f070f27d39ee52dff2da729d53e2fcc95ea4da
Bertrand Lecervoisier <miniwark@gmail.com>
2012-02-23 14:24:39 +0100

    Add a crsf token suscriber

--- wepwawet/lib/subscribers.py
+++ wepwawet/lib/subscribers.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
+from pyramid.events import BeforeRender, NewRequest
 from pyramid.i18n import get_localizer, TranslationStringFactory
 
 
 def includeme(config):
     """Configure subscribers."""
-    config.add_subscriber(add_renderer_globals, 'pyramid.events.BeforeRender')
-    config.add_subscriber(add_localizer, 'pyramid.events.NewRequest')
+    config.add_subscriber(add_renderer_globals, BeforeRender)
+    config.add_subscriber(add_localizer, NewRequest)
+    config.add_subscriber(csrf_validation, NewRequest)
 
 def add_renderer_globals(event):
     request = event.get('request')
@@ -21,3 +23,10 @@ def add_localizer(event):
         return localizer.translate(tsf(string))
     request.localizer = localizer
     request.translate = auto_translate
+
+
+def csrf_validation(event):
+    if event.request.method == "POST":
+        token = event.request.POST.get("csrf_token")
+        if token is None or token != event.request.session.get_csrf_token():
+            raise HTTPForbidden('CSRF token is missing or invalid')
