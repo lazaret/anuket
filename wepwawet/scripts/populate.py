534ec69a1d5ff7846ccb57b9ca0ec9e5e84fd943
Bertrand Lecervoisier <miniwark@gmail.com>
2012-03-09 23:16:33 +0100

    Add a default admin to the populate script

--- wepwawet/scripts/populate.py
+++ wepwawet/scripts/populate.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import sys
 import transaction
@@ -9,9 +10,9 @@ from pyramid.paster import (
     setup_logging,
     )
 
-from ..models import (
+from wepwawet.models import (
     DBSession,
-#    MyModel,
+    User,
     Base,
     )
 
@@ -31,6 +32,7 @@ def main(argv=sys.argv):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.create_all(engine)
-#    with transaction.manager:
-#        model = MyModel(name='one', value=1)
-#        DBSession.add(model)
+    with transaction.manager:
+        admin_user = User(
+            username='admin')
+        DBSession.add(admin_user)
