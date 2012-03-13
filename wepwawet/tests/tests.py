a2b0a4cc99cc761c5023afa7778c4421c4fbcb6a
Bertrand Lecervoisier <miniwark@gmail.com>
2012-03-13 12:34:48 +0100

    Add tests for the root view

--- wepwawet/tests/tests.py
+++ wepwawet/tests/tests.py
@@ -1,34 +1,53 @@
 # -*- coding: utf-8 -*-
 import unittest
-import transaction
+#import transaction
 from pyramid import testing
 
-from wepwawet.models import DBSession
+#from wepwawet.models import DBSession
 
 
-class ViewTests(unittest.TestCase):
+
+class MyTest(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
-        from sqlalchemy import create_engine
-        engine = create_engine('sqlite://')
-        from ..models import (
-            Base,
-#            MyModel,
-            )
-        DBSession.configure(bind=engine)
-        Base.metadata.create_all(engine)
-#        with transaction.manager:
-#            model = MyModel(name='one', value=55)
-#            DBSession.add(model)
+        # register routes
+        self.config.include('wepwawet.views.root')
 
     def tearDown(self):
-        DBSession.remove()
         testing.tearDown()
 
-    def test_my_view(self):
+
+    def test_01_root_view(self):
         from wepwawet.views.root import root_view
         request = testing.DummyRequest()
-        info = root_view(request)
-#        self.assertEqual(info['one'].name, 'one')
-        #TODO change this beacause brand_name can be modified
-#        self.assertEqual(info['brand_name'], 'Wepwawet')
+        response = root_view(request)
+        # test routes
+        self.assertEqual(request.route_path('home'), '/')
+        self.assertEqual(request.route_path('about'), '/about')
+        # test response
+        self.assertEqual(response, {})
+        #TODO: add tests for 404
+
+#    def test_02_forbident_view(self):
+#        #TODO: add tests for forbiden
+#        pass
+
+    def test_03_login_view_not_logged(self):
+        from wepwawet.views.root import login_view
+        request = testing.DummyRequest()
+        response = login_view(request)
+        # test route
+        self.assertEqual(request.route_path('login'), '/login')
+        # test response
+        self.assertIsNotNone(response['renderer'])
+
+
+    def test_05_logout_view(self):
+        from wepwawet.views.root import logout_view
+        request = testing.DummyRequest()
+        response = logout_view(request)
+        # test route
+        self.assertEqual(request.route_path('logout'), '/logout')
+        # test response
+        self.assertEqual(response.location, '/')
+        self.assertEqual(response.headers['Content-Length'], '0')
