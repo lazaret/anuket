89eeb4989fccc24e02f96b8ba16bb7727d1f4e01
Bertrand Lecervoisier <miniwark@gmail.com>
2012-03-19 21:04:24 +0100

    Update the initilizedb script to isolate initialization from arg parsing

--- wepwawet/scripts/initializedb.py
+++ wepwawet/scripts/initializedb.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+# -*- coding: utf-8 -*-
 import sys
 import transaction
 
@@ -9,27 +10,33 @@ from pyramid.paster import get_appsettings, setup_logging
 from wepwawet.models import DBSession, Base, AuthUser, AuthGroup
 
 
-def usage(argv):
+def usage(argv): #pragma: no cover
     cmd = os.path.basename(argv[0])
     print('usage: %s <config_uri>\n'
           '(example: "%s development.ini")' % (cmd, cmd))
     sys.exit(1)
 
-def main(argv=sys.argv):
+def initialize_db(config_uri=None):
+    """ Initialize the database with default values."""
+    if config_uri:
+        setup_logging(config_uri)
+        settings = get_appsettings(config_uri)
+        engine = engine_from_config(settings, 'sqlalchemy.')
+        DBSession.configure(bind=engine)
+        Base.metadata.create_all(engine)
+        with transaction.manager:
+            admins_group = AuthGroup(
+                groupname=u'admins')
+            DBSession.add(admins_group)
+            admin_user = AuthUser(
+                username=u'admin',
+                password=u'admin',
+                group=admins_group)
+            DBSession.add(admin_user)
+
+
+def main(argv=sys.argv): #pragma: no cover
     if len(argv) != 2:
         usage(argv)
     config_uri = argv[1]
-    setup_logging(config_uri)
-    settings = get_appsettings(config_uri)
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    DBSession.configure(bind=engine)
-    Base.metadata.create_all(engine)
-    with transaction.manager:
-        admins_group = AuthGroup(
-            groupname=u'admins')
-        DBSession.add(admins_group)
-        admin_user = AuthUser(
-            username=u'admin',
-            password=u'admin',
-            group=admins_group)
-        DBSession.add(admin_user)
+    initialize_db(config_uri)
