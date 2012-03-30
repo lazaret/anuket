32420bbfa24fc78e851072927d188374b79831ba
Bertrand Lecervoisier <miniwark@gmail.com>
2012-03-30 10:30:31 +0200

    Working on 0.4 version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ tests_require = [
 
 setup(
     name='anuket',
-    version='0.3',
+    version='0.4b',
     description='Anuket web application',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
