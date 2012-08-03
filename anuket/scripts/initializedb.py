e5899fdf5d433e7065f0d7b96d9cd7735c892bc3
Bertrand Lecervoisier <miniwark@gmail.com>
2012-08-03 09:27:22 +0200

    Add some logging messages

--- anuket/scripts/initializedb.py
+++ anuket/scripts/initializedb.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import argparse
+import logging
 import transaction
 
 from alembic.command import stamp
@@ -12,6 +13,9 @@ from anuket.lib.alembic_utils import get_alembic_settings
 from anuket.models import DBSession, Base, AuthUser, AuthGroup
 
 
+log = logging.getLogger(__name__)
+
+
 def initialize_db(config_uri=None):
     """ Initialize the database schema and add default values."""
     setup_logging(config_uri)
@@ -19,14 +23,19 @@ def initialize_db(config_uri=None):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     # check if there is already a versioned database
+    log.info("Checking for alembic revision")
     revision = get_alembic_revision(config_uri)
     if revision:
         message = "ERROR: This database is versioned. " \
                         "Please use the upgrade script instead!"
+        #log.error(message)
         return message
     # create the tables (except alembic_version)
+    log.info("Creating tables")
     Base.metadata.create_all(engine)
+    log.info("Tables creation done")
     # add default user & group values
+    log.info("Add default users & groups")
     with transaction.manager:
         admins_group = AuthGroup()
         admins_group.groupname = u'admins'
@@ -39,13 +48,18 @@ def initialize_db(config_uri=None):
             DBSession.add(admin_user)
             DBSession.flush()
             message = "Database initialization done."
+            #log.info("Adding default users & groups done")
         except IntegrityError:
             DBSession.rollback()
             message = "ERROR: An IntegrityError have occured"
+            #log.error(message)
+
     # stamp the database with the most recent revision
     # (and create alembic_version table)
+    log.info("Add alembic revision number")
     alembic_cfg = get_alembic_settings(config_uri)
     stamp(alembic_cfg, 'head')
+    log.info("Adding revision number done")
     return message
 
 
