fdcf312c091cb7633e1cf4bc6227245de03f6e14
Bertrand Lecervoisier <miniwark@gmail.com>
2012-08-03 16:14:05 +0200

    Comment the tests for scripts befor rewriting them

--- anuket/tests/test_script_initializedb.py
+++ anuket/tests/test_script_initializedb.py
@@ -24,50 +24,50 @@ class ScriptInitializedbTests(AnuketTestCase):
         version_table.drop(self.engine)
 
 
-    def test_01_initialize_db(self):
-        """ Test than the `initialize_db` function create the database and add
-        the default values.
-        """
-        from anuket.scripts.initializedb import initialize_db
-        message = initialize_db(config_uri)
-        from anuket.models import AuthUser
-        user = self.DBSession.query(AuthUser).filter_by().first()
-        self.assertEqual(user.username, u'admin')
-        self.assertTrue(AuthUser.check_password(u'admin', u'admin'))
-        self.assertEqual(user.group.groupname, u'admins')
-        self.assertEqual(message, "Database initialization done.")
+#    def test_01_initialize_db(self):
+#        """ Test than the `initialize_db` function create the database and add
+#        the default values.
+#        """
+#        from anuket.scripts.initializedb import initialize_db
+#        message = initialize_db(config_uri)
+#        from anuket.models import AuthUser
+#        user = self.DBSession.query(AuthUser).filter_by().first()
+#        self.assertEqual(user.username, u'admin')
+#        self.assertTrue(AuthUser.check_password(u'admin', u'admin'))
+#        self.assertEqual(user.group.groupname, u'admins')
+#        self.assertEqual(message, "Database initialization done.")
 
 
-    def test_02_initialize_db_with_revision_must_fail(self):
-        """ Test than the `initialize_db` fail if the database is versioned."""
-        import transaction
-        from anuket.models import Migration
-        version_table.create(self.engine)
-        with transaction.manager:
-            alembic_version = Migration()
-            alembic_version.version_num = 'revid'
-            self.DBSession.add(alembic_version)
-        self.DBSession.remove()
+#    def test_02_initialize_db_with_revision_must_fail(self):
+#        """ Test than the `initialize_db` fail if the database is versioned."""
+#        import transaction
+#        from anuket.models import Migration
+#        version_table.create(self.engine)
+#        with transaction.manager:
+#            alembic_version = Migration()
+#            alembic_version.version_num = 'revid'
+#            self.DBSession.add(alembic_version)
+#        self.DBSession.remove()
+#
+#        from anuket.scripts.initializedb import initialize_db
+#        message = initialize_db(config_uri)
+#        self.assertEqual(message, "ERROR: This database is versioned. "
+#                        "Please use the upgrade script instead!")
 
-        from anuket.scripts.initializedb import initialize_db
-        message = initialize_db(config_uri)
-        self.assertEqual(message, "ERROR: This database is versioned. "
-                        "Please use the upgrade script instead!")
 
-
-    def test_03_initialize_db_integrity_error(self):
-        """ Test than the `initialize_db` fail if an IntegrityError occur
-        when there is already an 'admins' group in the database."""
-        import transaction
-        from anuket.models import AuthGroup
-        Base.metadata.create_all()
-        with transaction.manager:
-            admins_group = AuthGroup()
-            admins_group.groupname = u'admins'
-            self.DBSession.add(admins_group)
-        self.DBSession.remove()
-
-        from anuket.scripts.initializedb import initialize_db
-        message = initialize_db(config_uri)
-        self.assertEqual(message, "ERROR: An IntegrityError have occured")
+#    def test_03_initialize_db_integrity_error(self):
+#        """ Test than the `initialize_db` fail if an IntegrityError occur
+#        when there is already an 'admins' group in the database."""
+#        import transaction
+#        from anuket.models import AuthGroup
+#        Base.metadata.create_all()
+#        with transaction.manager:
+#            admins_group = AuthGroup()
+#            admins_group.groupname = u'admins'
+#            self.DBSession.add(admins_group)
+#        self.DBSession.remove()
+#
+#        from anuket.scripts.initializedb import initialize_db
+#        message = initialize_db(config_uri)
+#        self.assertEqual(message, "ERROR: An IntegrityError have occured")
 
